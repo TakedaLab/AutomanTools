e029d2d23f94e58f69ed009ff3174003b1b1714c
Ryosuke Onose <dev@onose.ws>
2020-06-30 16:45:24 +0900

    add storybook addon, add sliders

--- front/app/labeling_tool/pcd_tool/base_edit_bar.jsx
+++ front/app/labeling_tool/pcd_tool/base_edit_bar.jsx
@@ -1,44 +1,162 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { RotateLeft, RotateRight } from '@material-ui/icons';
+import {
+  Button,
+  Divider,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Input,
+  Grid,
+  Slider,
+  Typography
+} from '@material-ui/core';
 
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import { RotateLeft, RotateRight, ExpandMore } from '@material-ui/icons';
 
-import { theme } from 'automan/assets/theme';
-import { mainStyle } from 'automan/assets/main-style';
+
+const InputSlider = props => {
+  const [value, setValue] = React.useState(30);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+  const {
+    label = "x",
+    disabled = false,
+  } = props;
+
+  return (
+    <div style={{width: "100%", display: "block"}}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <Typography style={{width: "64px", display: "block"}} variant="caption">
+            {label}
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+            disabled={disabled}
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            disabled={disabled}
+            style={{width: "64px"}}
+            value={value}
+            margin="dense"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
 
 export default class BasePCDEditBar extends React.Component {
   render() {
-    // const label = this.props.targetLabel;
-    // if (label == null) {
-    //   return null;
-    // }
-    // const bbox = label.bbox[this.props.candidateId];
-    // if (bbox == null) {
-    //   return null;
-    // }
+    const {
+      rotateFront,
+      disabled = false,
+      moveSelectedCube = () => {},
+      bbox_params = {
+        x: 0, y: 0, z: 0,
+        w: 1, h: 1, d: 1,
+        y: 1
+      }
+    } = this.props;
     return (
       <div>
-        <Divider />
+        <ExpansionPanel defaultExpanded={true}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMore />}
+          >
+            <Typography component="h3" variant="body1">
+              Bounding Box
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <div style={{width: "100%"}}>
+              <div style={{marginBottom: "32px"}}>
+                <Typography component="h4" variant="body1">
+                  Position
+                </Typography>
+                <InputSlider
+                  label="x"
+                  disabled={disabled}
+                />
+                <InputSlider
+                  label="y"
+                  disabled={disabled}
+                />
+                <InputSlider
+                  label="z"
+                  disabled={disabled}
+                />
+                <InputSlider
+                  label="yaw"
+                  disabled={disabled}
+                />
+              </div>
+              <div style={{marginBottom: "32px"}}>
+                <Typography component="h4" variant="body1">
+                  Size
+                </Typography>
+                <InputSlider
+                  label="depth"
+                  disabled={disabled}
+                />
+                <InputSlider
+                  label="width"
+                  disabled={disabled}
+                />
+                <InputSlider
+                  label="height"
+                  disabled={disabled}
+                />
+              </div>
+            </div>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+
         <Grid container>
           <Grid item xs={12}>
             Rotate Front
           </Grid>
           <Grid item xs={6}>
             <Button
-              onClick={() => bbox.rotateFront(1)}
+              onClick={() => rotateFront(1)}
             >
               <RotateLeft />
             </Button>
           </Grid>
           <Grid item xs={6}>
             <Button
-              onClick={() => bbox.rotateFront(-1)}
+              onClick={() => rotateFront(-1)}
             >
               <RotateRight />
             </Button>
--- front/app/labeling_tool/pcd_tool/base_edit_bar.stories.jsx
+++ front/app/labeling_tool/pcd_tool/base_edit_bar.stories.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import { action } from '@storybook/addon-actions'
 
 import BasePCDEditBar from './base_edit_bar'
 
 storiesOf('pcd_tool/base_edit_bar', module)
+  .addDecorator(withKnobs)
   .add('default', () =>
     <div>
-      <BasePCDEditBar />
+      <BasePCDEditBar
+        disabled={boolean("Disabled", false)}
+        rotateFront={action('button-click')}
+      />
     </div>
   )
