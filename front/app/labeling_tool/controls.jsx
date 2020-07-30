f39c79d1afd58a474521c9f2fdcda841ece9aa06
Yusuke <y.f.sakai@gmail.com>
2020-07-30 15:34:53 +0900

    Fix prefetching previous frame

--- front/app/labeling_tool/controls.jsx
+++ front/app/labeling_tool/controls.jsx
@@ -455,13 +455,9 @@ class Controls extends React.Component {
       })
       // Load previous frame data for wipe
       .then(() => {
-        // FIXME: Load previous frame with skip step like below, but need to fix the previous frame wipe bug first
-        // const previousFrameNumber = num - Math.max(1, this.state.skipFrameCount);
-        // if (this.validateFrameNumber(previousFrameNumber)) {
-        //   return this.props.labelTool.loadBlobURL(previousFrameNumber);
-        // }
-        if (num > 1) {
-          return this.props.labelTool.loadBlobURL(num - 1);
+        const previousFrameNumber = num - Math.max(1, this.state.skipFrameCount);
+        if (this.validateFrameNumber(previousFrameNumber)) {
+          return this.props.labelTool.loadBlobURL(previousFrameNumber);
         }
       })
       .then(() => {
