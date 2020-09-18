97134625d7daa4d9a859d0370c3de2aa5705d953
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-09-18 20:59:49 +0900

    Bug fix

--- front/app/labeling_tool/pcd_label_tool.jsx
+++ front/app/labeling_tool/pcd_label_tool.jsx
@@ -250,6 +250,9 @@ class PCDLabelTool extends React.Component {
   pcdLoad(frame) {
     if (this.isLoadedFrame(frame)) {
       this._pointMeshes[frame].geometry.colors = this.colorPoints(this._pointMeshes[frame].geometry.vertices);
+      this._pointMeshes[frame].geometry.colorsNeedUpdate = true;
+      this._wipePointMeshes[frame].geometry.colors = this.colorPoints(this._wipePointMeshes[frame].geometry.vertices);
+      this._wipePointMeshes[frame].geometry.colorsNeedUpdate = true;
       return Promise.resolve({
         mesh: this._pointMeshes[frame],
         wipeMesh: this._wipePointMeshes[frame]
